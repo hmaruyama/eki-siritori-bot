b43104869dd3335893bbf26c0072f6834c35c2bf
hmaruyama <maruyama@val.co.jp>
2017-07-28 13:13:49 +0900

    インデントを空白2文字に統一しました。

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var request = require('request');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
+  console.log('%s listening to %s', server.name, server.url);
 });
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 // Listen for messages from users
@@ -20,61 +20,61 @@ server.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
 
-    baseUrl = "http://api.ekispert.jp/v1/json/"
-    method = "station"
-    url = encodeURI(baseUrl + method + "?name=" + session.message.text + "&key=" + process.env.EKISPERT_ACCESS_KEY)
-    options = {
-      url: url,
-      json: true
-    }
+  baseUrl = "http://api.ekispert.jp/v1/json/"
+  method = "station"
+  url = encodeURI(baseUrl + method + "?name=" + session.message.text + "&key=" + process.env.EKISPERT_ACCESS_KEY)
+  options = {
+    url: url,
+    json: true
+  }
 
-    request.get(options, function(error, response, body) {
-      if (!error && response.statusCode == 200) {
+  request.get(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
 
-        if (!body.ResultSet.Point) {
-          session.send("そんなえきないですよ")
-          return
-        }
-
-        point = {}
-        if (body.ResultSet.Point instanceof Array) {
-          point = body.ResultSet.Point[0]
-        } else {
-          point = body.ResultSet.Point
-        }
-
-        if (point.Station.Yomi.slice(-1) == 'ん') {
-          session.send('さいごに"ん"がついてるよ！')
-          return
-        }
-
-        url = encodeURI(baseUrl + method + "?name=" + point.Station.Yomi.slice(-1) + "&key=" + process.env.EKISPERT_ACCESS_KEY)
-        options = {
-          url: url,
-          json: true
-        }
-        request.get(options, function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-
-            if (!body.ResultSet.Point) {
-              session.send('まいりました')
-              return
-            }
-
-            point = {}
-            if (body.ResultSet.Point instanceof Array) {
-              point = body.ResultSet.Point[0]
-            } else {
-              point = body.ResultSet.Point
-            }
-            session.send("みつけました！ %s です", point.Station.Yomi)
-          } else {
-            console.log('error: '+ response.statusCode)
-          }
-        })
-
-      } else {
-        console.log('error: '+ response.statusCode)
+      if (!body.ResultSet.Point) {
+        session.send("そんなえきないですよ")
+        return
       }
-    })
+
+      point = {}
+      if (body.ResultSet.Point instanceof Array) {
+        point = body.ResultSet.Point[0]
+      } else {
+        point = body.ResultSet.Point
+      }
+
+      if (point.Station.Yomi.slice(-1) == 'ん') {
+        session.send('さいごに"ん"がついてるよ！')
+        return
+      }
+
+      url = encodeURI(baseUrl + method + "?name=" + point.Station.Yomi.slice(-1) + "&key=" + process.env.EKISPERT_ACCESS_KEY)
+      options = {
+        url: url,
+        json: true
+      }
+      request.get(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+          if (!body.ResultSet.Point) {
+            session.send('まいりました')
+            return
+          }
+
+          point = {}
+          if (body.ResultSet.Point instanceof Array) {
+            point = body.ResultSet.Point[0]
+          } else {
+            point = body.ResultSet.Point
+          }
+          session.send("みつけました！ %s です", point.Station.Yomi)
+        } else {
+          console.log('error: '+ response.statusCode)
+        }
+      })
+
+    } else {
+      console.log('error: '+ response.statusCode)
+    }
+  })
 });
